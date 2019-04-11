a145a8336dd9a64b721cd1c552b0369c766c515d
Adrian Radillo <adrian.radillo@gmail.com>
2019-04-11 15:46:38 -0400

    compute average ME across dataset

--- Python/modules/ME_functions.py
+++ Python/modules/ME_functions.py
@@ -1,67 +1,11 @@
-import numpy as np
-import matplotlib.pyplot as plt
-# from moviepy.editor import VideoClip
-# from moviepy.video.io.bindings import mplfig_to_npimage
+# import numpy as np
+# import matplotlib.pyplot as plt
+import pandas as pd
+import motionenergy as kiani_me
+import sys
+sys.path.insert(0, 'dots_db/dotsDB/')
+import dotsDB as ddb
 
-# def custom_ME(stateMatrix, displayMatrices=False):
-#     N = stateMatrix.shape[1] # number of cells in 1D space
-    
-#     # compute displacement 
-#     displacementMatrix = np.diff(stateMatrix, axis=0)
-
-#     # compute movement matrix
-#     movementMatrix = np.diff(displacementMatrix, axis=1)
-
-#       # boolean for ulterior indexing
-#     toKillLeft = displacementMatrix[:,:(N-1)] == 0
-#     toKillRight = displacementMatrix[:,1:] == 0
-
-#       # set to zero movement occurring where the left cell was stationary
-#     movementMatrix[toKillLeft] = 0
-
-#       # set to zero movement occurring where the right cell was stationary
-#     movementMatrix[toKillRight] = 0 
-
-#     # inspect matrices
-#     if displayMatrices:
-#         print('state matrix\n',stateMatrix)
-#         print('displacement matrix\n',displacementMatrix)
-#         print('movement matrix\n',movementMatrix)
-
-#     # compute motion energy (custom definition explained in above sections)
-#     return np.sum(movementMatrix, axis=1)
-
-# def map_time_to_frame(t, binSize):
-#     return int(np.floor(t/binSize))
-
-# def map_frame_number_to_time_idx(frameNumber, T):
-#     return np.mod(frameNumber,T+1)
-
-# ''' --- I didn't manage to put this function here instead of in the notebook :(
-# def make_frame(t, stateMatrix, ME, binSize, fig, ax1, ax2):
-
-#     # define quantities used later on
-#     _, N = stateMatrix.shape  # N is number of cells
-#     x = np.arange(N)+1        # 1D space
-#     T = _ - 1                 # T is time horizon (T = number of time steps - 1)
-
-#     frameNumber = map_time_to_frame(t, binSize)    
-#     time_idx = map_frame_number_to_time_idx(frameNumber, T)
-    
-#     # plot particles
-#     ax1.clear()
-
-#     xx=x[stateMatrix[time_idx,:]==1]
-#     ax1.plot(xx, 0, 'bo')
-#     ax1.set_ylim(-.5, 0.5)
-#     ax1.set_xlim(0, N+1)
-    
-#     #ax2.clear()
-#     if time_idx > 0:  # account for the fact that ME doesn't exist at time = 0
-#         ax2.plot(frameNumber,ME[time_idx - 1],'bo')
-
-#     return mplfig_to_npimage(fig)
-# '''
 
 def map_snow_dots_params_to_kiani_dots(param_dict):
     new_dict = {}
@@ -76,3 +20,61 @@ def map_snow_dots_params_to_kiani_dots(param_dict):
     new_dict['moments'] = None # irrelevant for now
     new_dict['seed'] = param_dict['randSeedBase']
     return new_dict
+
+
+def compute_motion_energy_for_trials_in_db(db_file, dset_name, gp_name, trial_list, filters, append_to=None):
+    """
+
+    :param db_file: full path to hdf5 file created with the dotsDB module
+    :param dset_name: name of dataset inside hdf5 file (full path)
+    :param gp_name: name of group inside hdf5 file (full path)
+    :param trial_list: list of positive integers indicating the trials to extract
+    :param filters: filters as outputted by motion_filters()
+    :param append_to: pandas data frame. If None, a data frame is created, otherwise, rows are appended.
+    :return: a pandas data frame in long format with the following columns
+        dsetID; filtersID; trial; time; ME; direction; coherence; density
+        If append_to is not None, the inputted data frame is edited in place?
+    """
+    db_info = ddb.inspect_db(db_file)
+    attrs_dict = dict(db_info[gp_name]['attrs'])
+
+    trials = [ddb.extract_trial_as_3d_array(db_file, dset_name, gp_name, trial_number) for trial_number in trial_list]
+    dots_energy = [kiani_me.apply_motion_energy_filters(x, filters) for x in trials]
+
+    time_points = kiani_me.filter_grid(attrs_dict['num_frames'], 1 / attrs_dict['frame_rate'])
+    assert len(time_points) > 0
+    # the data frame to return has num_trials x num_time_points rows
+
+    # list of dict that will become a data frame
+    rows = []
+
+    dataset_id = create_dset_id(db_file, dset_name)
+    filters_id = create_filters_id(filters)
+
+    for trial in range(len(trial_list)):
+        motion_energy = dots_energy[trial].sum(axis=(0, 1))
+        for time_point in range(len(time_points)):
+            row_as_dict = {
+                'dsetID': dataset_id,
+                'filtersID': filters_id,
+                'trial': trial_list[trial],
+                'time': time_points[time_point],
+                'ME': motion_energy[time_point],
+                'direction': attrs_dict['direction'],
+                'coherence': attrs_dict['coh_mean'],
+                'density': attrs_dict['density']
+            }
+            rows.append(row_as_dict)
+
+    if append_to is None:
+        return pd.DataFrame(rows)
+
+
+def create_dset_id(db_file, dset_name):
+    """todo: to write"""
+    return ''
+
+
+def create_filters_id(filters):
+    """todo: to write"""
+    return ''
